d62856fad1448098ecd9dd1e7cab41dbce0aa9ce
Matt Michel <matt@ninjaPixel.io>
2024-03-21 09:18:20 +0000

    Add 27. Remove Element

---
+++ src/27-remove-element/27.ts
@@ -0,0 +1,21 @@
+// 27. Remove Element
+// https://leetcode.com/problems/remove-element
+
+/**
+ * Solve
+ */
+// 🟡 Add the function or class skeleton from LeetCode here.
+ 
+/**
+ * Test
+ */
+describe("27. Remove Element", () => {
+    it("example 1", () => {
+        // 🟡 Add test case here.
+        const input = false;
+        expect(input).toBe(true);
+    });
+});
+
+// Make this file a module to avoid global scoping
+export { }
